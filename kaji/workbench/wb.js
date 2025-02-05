3e59c3169812aaf5ff535d88d6e63c58f3876099
Mr.Xie <1034457629@qq.com>
2025-02-05 16:42:57 +0800

    1

--- kaji/workbench/wb.js
+++ kaji/workbench/wb.js
@@ -54,6 +54,7 @@ const noneSvgCode2 = `
 </svg>
 `;
 //删除图
+//这是注释
 const deleteSvgCode = `
 <svg t="1731562937821" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2642" width="20" height="20">
     <defs>
